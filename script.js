bfd9276b751452d7382bed537c9037ffd9dacbd0
Bukaj03 <jakub.ogorkis.code@outlook.com>
2022-05-05 09:43:36 +0200

    added-animations-to-portfolio-images

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-function modal(x){
-    let modal = document.getElementsByClassName('portfolio-gallery-modal')[0];
-    let img = document.getElementsByClassName('portfolio-gallery-link-image')[x-1];
-    let modalImg = document.getElementsByClassName('portfolio-gallery-modal-img')[0];
-    modal.style.display = 'flex';
-    modalImg.src = img.src;
-    modalImg.alt = x;
+function modal(x, y){
+    let modal = document.getElementsByClassName('modal')[y];
+    let modalImg = document.getElementsByClassName('modal-content')[y]
+    let captionText = document.getElementsByClassName("caption")[y];
+    modal.style.display = "flex";
+    modalImg.src = x.src;
+    captionText.innerHTML = x.alt;
 }
+function modalClose(y){
+    let modal = document.getElementsByClassName('modal')[y];
+    modal.style.display = "none";
+}
+
+
 function openMenu(){
     document.getElementsByClassName('menu')[0].style.display = 'block';
     document.getElementsByClassName('close-menu')[0].style.display = 'block';
